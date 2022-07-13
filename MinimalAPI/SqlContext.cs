c72d8d14762a579f7b33d3194bf6540f64179e2e
Narriman Lima <narrimancslima@gmail.com>
2022-07-13 14:09:43 -0300

    Feat: DataContext

---
+++ MinimalAPI/SqlContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MinimalAPI
+{
+    public class SqlContext : DbContext
+    {
+        public SqlContext(DbContextOptions<SqlContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Person> People => Set<Person>();
+    }
+}
